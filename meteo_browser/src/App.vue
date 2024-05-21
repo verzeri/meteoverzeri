af3cfc69df6675f33d4ca3946a8b4357a32e551b
verzeri <134490405+verzeri@users.noreply.github.com>
2024-05-21 09:34:43 +0000

    secondo

--- meteo_browser/src/App.vue
+++ meteo_browser/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">Tabelle</router-link> |
+    <router-link to="/about">Grafici</router-link>
   </nav>
   <router-view/>
 </template>
--- meteo_browser/src/views/HomeView.vue
+++ meteo_browser/src/views/HomeView.vue
@@ -1,18 +1,45 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <table>
+      <thead>
+        <tr>
+          <th>Città</th>
+          <th v-for="year in years" :key="year">{{ year }}</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="row in data" :key="row.Comune">
+          <td>{{ row.Comune }}</td>
+          <td v-for="year in years" :key="year">{{ row[year] }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      years: [2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021],
+      data: [
+        {"Comune": "Torino", "2006": 15.02, "2007": 15.43, "2008": 14.55, "2009": 15.09, "2010": 13.87, "2011": 15.37, "2012": 14.97, "2013": 14.41, "2014": 15.28, "2015": 14.79, "2016": 15.31, "2017": 14.67, "2018": 14.95, "2019": 15.2, "2020": 14.67, "2021": 14.56},
+        {"Comune": "Vercelli", "2006": 13.19, "2007": 13.36, "2008": 13.34, "2009": 13.49, "2010": 12.54, "2011": 13.86, "2012": 13.42, "2013": 13.15, "2014": 14.21, "2015": 13.95, "2016": 13.85, "2017": 14.03, "2018": 13.78, "2019": 13.77, "2020": 13.42, "2021": 13.67}
+        // Aggiungi altri comuni qui...
+      ]
+    };
   }
-}
+};
 </script>
+
+<style scoped>
+table {
+  width: 100%;
+  border-collapse: collapse;
+}
+th, td {
+  border: 1px solid black;
+  padding: 8px;
+  text-align: left;
+}
+</style>
