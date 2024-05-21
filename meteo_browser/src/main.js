8ce794ade300a02fc75ddac856f729177e7b64c0
verzeri <134490405+verzeri@users.noreply.github.com>
2024-05-21 08:48:51 +0000

    primo

---
+++ meteo_browser/src/main.js
@@ -0,0 +1,21 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+//import { useDisplay } from 'vuetify';
+//import Hls from 'hls.js';
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import VueApexCharts from "vue3-apexcharts";
+
+const app = createApp(App);
+app.use(VueApexCharts);
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+createApp(App).use(router).use(vuetify).mount('#app')
